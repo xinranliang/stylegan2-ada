f72e2810b5aaf0667f19f9c94ce6218a4d158581
Xinran Liang <xinranliang@princeton.edu>
2023-04-27 12:24:16 -0400

    scripts

--- scripts/sample_cifar.sh
+++ scripts/sample_cifar.sh
@@ -1,3 +1,2 @@
-
-CUDA_VISIBLE_DEVICES=0 python generate.py --outdir=./logs/cifar --num-classes=10 --num-samples=100000 --batch-size=500 \
+CUDA_VISIBLE_DEVICES=0 python generate.py --outdir=./logs/samples/cifar --num-classes=10 --num-samples=100000 --batch-size=500 \
     --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl
